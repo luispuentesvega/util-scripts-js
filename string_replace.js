8c3437f6f6a8abd81589d337ddc02b479679be03
Luis Puentes <luispuentesvega@gmail.com>
2018-03-27 19:29:46 +0700

    New Functions

--- string_replace.js
+++ string_replace.js
@@ -1,9 +1,14 @@
 //1.Replace just the first match
 let myStr = 'My little Alligator 🐊 I have a little Alligator';
-console.log(myStr.replace('Alligator', 'gator'));
+//console.log(myStr.replace('Alligator', 'gator'));
 //Output: My little gator 🐊 I have a little Alligator
 
 //1.Replace all the matches
-str = myStr.replace(new RegExp('Alligator', 'g'), 'gator');
-console.log('Final:',str);
+str = myStr.replace(new RegExp('Alligator','g'), 'gator').replace(new RegExp('a', 'g'), 'e');
+//console.log('Final:',str);
 //Output: Final: My little gator 🐊 I have a little gator
+
+
+let [new_first_word, new_website] = "11ww111云顶彩吧qqqqqqqqqqwww.google.com".replace(/www./,'&').split('&');
+console.log('first_word:',new_first_word);
+console.log('website:',new_website);
