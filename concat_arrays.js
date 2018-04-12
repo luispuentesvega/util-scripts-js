6db048bc52a7d59c779486f7964175aff67f6ad4
Luis Puentes <luispuentesvega@gmail.com>
2018-04-12 14:11:45 +0700

    Concat

---
+++ concat_arrays.js
@@ -0,0 +1,6 @@
+let animals = ['cocodrile', 'cat', 'dog']
+let fishes = ['shark', 'dolphin', 'monkey']
+
+let newArray = animals.concat(fishes);
+console.log(newArray )
+//Output: [ 'cocodrile', 'cat', 'dog', 'shark', 'dolphin', 'monkey' ]
