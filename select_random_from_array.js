ad68fd27b71c19359c03c94e8d6f12b4c8864cad
Luis Puentes <luispuentesvega@gmail.com>
2018-07-11 20:42:12 +0700

    Improving select random from Array

--- select_random_from_array.js
+++ select_random_from_array.js
@@ -1,7 +1,7 @@
 var countries = ["Colombia", "Venezuela", "Canada", "Ecuador", "USA", "China", "Cambodia", "Vietnam"];
 
-function pickRandomCountry(countries){
-    var index = Math.floor(Math.random() * (countries.length));
+function pickRandomCountry(countries) {
+    var index = Math.floor(Math.random() * countries.length);
     return countries[index];
 }
 
