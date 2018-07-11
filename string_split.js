d7f7a8f09929384fe8e5139bd244e239b9a56650
Luis Puentes <luispuentesvega@gmail.com>
2018-07-11 20:38:23 +0700

    Split

--- string_split.js
+++ string_split.js
@@ -1,8 +1,4 @@
 let str = 'Hello  How  are you ? My N ame is    Luis';
 let arr = str.split(' ');
-let tmpArr = arr.filter(item=>{
-    if(item.trim().length>0){
-        return item;
-    }
-});
+let tmpArr = arr.filter(item=>item.trim().length>0);
 console.log(tmpArr);
