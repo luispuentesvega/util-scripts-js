129e447eeacd43c0478bac8d5b8728cd744a7cb5
“luispuentesvega” <luispuentesvega@gmail.com>
2018-07-20 18:48:55 +0700

    Know if a variable is JSON

---
+++ know_if_is_a_json.js
@@ -0,0 +1,17 @@
+function isJson(item) {
+  item = typeof item !== "string"
+      ? JSON.stringify(item)
+          : item;
+  try {
+          item = JSON.parse(item);
+  } catch (e) {
+      return false;
+  }
+  if (typeof item === "object" && item !== null) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isJson('{"Luis"}'));//False
+console.log(isJson('{ "name":"John", "age":30, "car":null }'));//True
