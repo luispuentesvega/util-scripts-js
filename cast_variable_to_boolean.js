a6fee9db08c28bf97bd26bade833cb6f1ed1eea5
Luis Puentes <luispuentesvega@gmail.com>
2018-11-27 15:10:00 +0700

    Improving cast variable to boolean

--- cast_variable_to_boolean.js
+++ cast_variable_to_boolean.js
@@ -1,8 +1,17 @@
-let var1 = 0;
+let var1 = "0";
 let var2 = 1;
 let var3 = '';
 let var4 = 'Hello';
-console.log(!!var1);//False
-console.log(!!var2);//True
-console.log(!!var3);//False
-console.log(!!var4);//True
+let var5 = "1";
+let var6 = null;
+let var7 = undefined;
+
+const isBoolean = str => Boolean(parseInt(str));
+
+console.log(isBoolean(var1));//False
+console.log(isBoolean(var2));//True
+console.log(isBoolean(var3));//False
+console.log(isBoolean(var4));//False
+console.log(isBoolean(var5));//True
+console.log(isBoolean(var6));//False
+console.log(isBoolean(var7));//False
