71046bd7542fafedd8041188a8013da5d3fee3c7
Luis Puentes <luispuentesvega@gmail.com>
2019-01-18 14:03:42 +0700

    Add new function

--- get_filename_from_url.js
+++ get_filename_from_url.js
@@ -1,3 +1,3 @@
-const url = 'http://api.ruokdev.co:9999/upload/2019-01/0_20190118021003.jpg';
+const url = 'http://www.google.com/0_20190118021003.jpg';
 const fileName = url.substring(url.lastIndexOf('/')+1);
 console.log(fileName);
